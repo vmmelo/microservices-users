b8f00895651dabf9b9e9be0ab43fe53d8f3d2f14
Victor Miranda <vmmelo1@gmail.com>
2022-07-22 03:25:41 -0300

    feat: http_port env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = parseInt(process.env.HTTP_PORT) || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
-bootstrap();
+
+bootstrap().then(() => console.log(`Server running on port ${port}`));
